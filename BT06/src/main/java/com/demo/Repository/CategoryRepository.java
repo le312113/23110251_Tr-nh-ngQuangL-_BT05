c9d39bf06adc6ee6b19f1b28bc4226fb0693a80d
le312113 <23110251@student.hcmute.edu.vn>
2025-09-18 16:40:58 +0700

    .

--- BT06/src/main/java/com/demo/Repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.Repository;
-
-import com.demo.Entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-    Category findById(int id);
-}
--- BT06/src/main/java/com/demo/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.Repository;
-
-import com.demo.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserName(String username);
-    User findById(int id);
-}
